4d2461146447284683c12ae6da7729c3846fed99
Shiv <70622723+ShivShankarDGH@users.noreply.github.com>
2020-09-04 09:27:48 +0530

    build

---
+++ build/precache-manifest.8c4062f7d1cfad1a27d159f2bf8a8351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316e84038f9894c30955f53710a65fde",
+    "url": "/web_api/index.html"
+  },
+  {
+    "revision": "a5c5b0f8dba6eccab7c4",
+    "url": "/web_api/static/css/main.28181919.chunk.css"
+  },
+  {
+    "revision": "8fa59b3adbb8ac64adc6",
+    "url": "/web_api/static/js/2.ab3216ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web_api/static/js/2.ab3216ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c5b0f8dba6eccab7c4",
+    "url": "/web_api/static/js/main.47b5b391.chunk.js"
+  },
+  {
+    "revision": "1374d8fba57e22cdb73a",
+    "url": "/web_api/static/js/runtime-main.19ae6f0f.js"
+  }
+]);
